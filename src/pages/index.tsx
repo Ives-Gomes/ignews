d927ee209c391e7e0c442cadd2da37a3c99baf97
Ives Moreira Gomes <ivesmoreira3@gmail.com>
2022-08-10 14:04:35 -0300

    feat: add global styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,15 @@
 import type { NextPage } from 'next'
-
-import styles from '../styles/home.module.scss'
+import Head from 'next/head'
 
 const Home: NextPage = () => {
   return (
-    <h1 className={styles.title}>Next</h1>
+    <>
+      <Head>
+        <title>Início | ig.news</title>
+      </Head>
+
+      <h1>Next</h1>
+    </>
   )
 }
 
