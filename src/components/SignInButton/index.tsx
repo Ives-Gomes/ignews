eb691b715518cea23f8672ce57da9546859e5182
Ives Moreira Gomes <ivesmoreira3@gmail.com>
2022-08-11 15:19:24 -0300

    feat: add oauth with github

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,19 +1,24 @@
 import type { NextPage } from 'next'
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
+import { signIn, signOut, useSession } from 'next-auth/react'
 
 import styles from './styles.module.scss'
 
 const SignInButton: NextPage = () => {
-  const isUserLoggedIn = true;
+  const session = useSession();
 
   return (
-    <button type='button' className={styles.signInButton}>
+    <button 
+      type='button' 
+      className={styles.signInButton}
+      onClick={session.data ? () => signOut() : () => signIn('github') }
+    >
       <FaGithub 
-        color={isUserLoggedIn ? '#04D361' : '#EBA417'} 
+        color={session.data ? '#04D361' : '#EBA417'} 
       />
-      {isUserLoggedIn ? 'Ives Moreira' : 'Sign in with GitHub'}
-      {isUserLoggedIn && <FiX color='#737380' className={styles.closeIcon} />}
+      {session.data ? session.data.user?.name : 'Sign in with GitHub'}
+      {session.data && <FiX color='#737380' className={styles.closeIcon} />}
     </button>
   );
 };
