d4f315aabd39171f12dd13dc5210ded971216dc2
Ives Moreira Gomes <ivesmoreira3@gmail.com>
2022-08-11 17:37:40 -0300

    feat: create user in db only if not exists

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,7 @@
 import NextAuth from 'next-auth'
 import GithubProvider from 'next-auth/providers/github'
 
-import { query as q } from 'faunadb'
+import { ExprArg, query as q } from 'faunadb'
 
 import { fauna } from '@service/fauna'
 
@@ -23,9 +23,25 @@ export default NextAuth({
 
     try {
       await fauna.query(
-        q.Create(
-          q.Collection('users'),
-          { data: { email }},
+        q.If(
+          q.Not(
+            q.Exists(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email as ExprArg)
+              )
+            )
+          ),
+          q.Create(
+            q.Collection('users'),
+            { data: { email }},
+          ),
+          q.Get(
+            q.Match(
+              q.Index('user_by_email'),
+              q.Casefold(user.email as ExprArg)
+            )
+          )
         )
       )
 
