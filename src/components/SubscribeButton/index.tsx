f07a238043f33d3b96f25e39a4d8c3734d8b7701
Ives Moreira Gomes <ivesmoreira3@gmail.com>
2022-08-11 18:15:27 -0300

    feat: add redirect to stripe payment

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,5 +1,8 @@
 import { useSession, signIn } from 'next-auth/react'
 
+import { api } from '@service/api';
+import { getStripeJs } from '@service/stripe-js';
+
 import styles from './styles.module.scss'
 
 interface SubscribeButtonProps {
@@ -9,20 +12,33 @@ interface SubscribeButtonProps {
 const SubscribeButton = ({ priceId }: SubscribeButtonProps) => {
   const session = useSession()
 
-  function handleSubscribe() {
+  async function handleSubscribe() {
     if (!session) {
       signIn('github')
 
       return
     }
 
-    
+    try {
+      const response = await api.post('/subscribe')
+
+      const { sessionId } = response.data
+
+      const stripe  = await getStripeJs()
+
+      await stripe?.redirectToCheckout({ sessionId })
+    } catch (err: any) {
+      console.log(err)
+
+      alert(err.message)
+    }
   }
 
   return (
     <button
       type='button'
       className={styles.subscribeButton}
+      onClick={handleSubscribe}
     >
       Subscribe now
     </button>
